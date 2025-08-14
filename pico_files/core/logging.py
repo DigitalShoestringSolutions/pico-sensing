6359246ab98b6f0cec104d93c0c8ed0c517bc663
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2025-08-14 18:45:07 +0100

    v0.3.0 release (#21)
    * Feature/microseconds (#16)
    
    * Added fractional seconds to ISO8601 timestrings
    
      microsecond precision.
      Also made test repetitive.
      Switched logging.py to use external timestring
    
    * Truncate log messages over 200 characters (#19)
    
    * Compensation for cycle run time (#20)
    
    * Feature/adxl345 accelerometer and FFT (#18)
    
      Initial play with ADXL345
      Added Peter Hinch's DFT in assembler
      Downsample FFT results

--- pico_files/core/logging.py
+++ pico_files/core/logging.py
@@ -5,7 +5,7 @@ Logging system for Micropython.
 Public API: the function log()
 
 No log levels. Include them in your message if you want to.
-Messages are cast to string, timestamped (second precision) and printed to serial and also written to file.
+Messages are cast to string, timestamped (microsecond resolution) and printed to serial and also written to file.
 Automatic file size limitation, rotation and deletion.
 
 sample usage:
@@ -16,15 +16,17 @@ log(mymsg)
 Inspired by http://www.d3noob.org/2024/03/logging-and-troubleshooting-on.html
 """
 
-# standard imports
+# Standard library imports
 import os
-from machine import RTC
+
+# Local imports
+from core.timestamp import get_timestamp
 
 # settings
 log_filename = "log.txt"
 log_filename_old = "log_old.txt"
 log_file_size_limit = 200000 # Bytes before rotating. 200kB, so both log files toogether will take max 20% of Pico's 2MB flash memory.
-
+default_max_loginfo_size = 200 # Characters before log message is truncated
 
 def _create_log_file():
     """Check to see if primary log file is present and create if necessary."""    
@@ -38,20 +40,25 @@ def _create_log_file():
 
 
 # Public function
-def log(loginfo:str, print_loginfo:bool=True):
-    """Append a message to the plaintext log file. A timestamp from rtc.datetime() is added.
+def log(loginfo:str, print_loginfo:bool=True, max_loginfo_size=default_max_loginfo_size):
+    """Append a message to the plaintext log file. A timestamp from core.timestamp (rtc.datetime() + time_ns()) is added.
     
-    :param str loginfo:             The message to be appended to the log file.
-    :param bool=True print_loginfo: (optional) If `True`, loginfo will be passed to print() to display in the serial terminal
+    :param str loginfo:              The message to be appended to the log file.
+    :param bool=True print_loginfo:  (optional) If `True`, loginfo will be passed to print() to display in the serial terminal
+    :param int=200 max_loginfo_size: (optional) The number of characters after which str(loginfo) will be truncated
     """
 
-    # Format the timestamp
-    timestamp=rtc.datetime()
-    timestring="%04d-%02d-%02d %02d:%02d:%02d"%(timestamp[0:3] + timestamp[4:7])
+    # Get the timestamp as soon as called
+    timestring = get_timestamp()
 
     # Check the file size
     filestats = os.stat(log_filename)
     filesize = filestats[6]           # File size in bytes
+
+    # Truncate loginfo if long
+    loginfo = str(loginfo) # if something other than a string is supplied, attempt to cast it.
+    if len(loginfo) > max_loginfo_size:
+        loginfo = loginfo[:max_loginfo_size] + "..."
 
     # Print to terminal if required
     if print_loginfo:
@@ -61,7 +68,7 @@ def log(loginfo:str, print_loginfo:bool=True):
         try:
 
             # loginfo is recast to string. Add filesize (right aligned, padded to fit maximum)
-            logline = timestring +" "+ f"{str(filesize):>{len(str(log_file_size_limit))}}"  +" "+ str(loginfo) +"\n"
+            logline = timestring +" "+ f"{str(filesize):>{len(str(log_file_size_limit))}}"  +" "+ loginfo +"\n"
 
             # Write to file
             with open(log_filename, "at") as f:
@@ -85,7 +92,6 @@ def log(loginfo:str, print_loginfo:bool=True):
 
 
 # Startup
-rtc = RTC()
 _create_log_file() # Although when writing the file would be created as needed, this is done first so the filesize can be checked without error.
 
 
